25756091eb115f63e607ff579a608bc8e9e8152d
zliu9 <zhanglin.liu@intel.com>
2014-08-28 13:58:27 +0800

    - added app.js and package.json

---
+++ test/fish/app.js
@@ -0,0 +1,27 @@
+var fs = require('fs');
+var child_process = require('child_process');
+var path = require('path');
+
+var io = require('../../main/highLevelAPI/io.js');
+var sys = require('../../main/highLevelAPI/sys.js');
+
+var logPrefix = '[app fish] '
+
+var appProcess = child_process.execFile(path.join(__dirname, 'fish'));
+appProcess.on('close', function (code, signal) {
+});
+
+// Touch event handler begin
+// For none js app only
+io.touchPanel.appHandleEscape = true;
+io.touchPanel.on('touchEvent', function(e, x, y, id) {
+  if (e == 'TOUCH_HOLD') {
+    //console.log(logPrefix+'kill the main app pid='+appProcess.pid);
+    try {
+      process.kill(appProcess.pid);
+    } catch (ex) {
+    }
+    sys.escape();
+    process.exit();
+  }
+});
