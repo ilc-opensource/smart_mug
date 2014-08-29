9da2adbab1015ecef4cdf7d0c994616cd5faaf0d
zliu9 <zhanglin.liu@intel.com>
2014-08-29 11:10:25 +0800

    added release build

--- common.mk
+++ common.mk
@@ -2,7 +2,11 @@ LD_FLAGS=-L$(ROOT)/bin -lmug -lpthread -lrt
 
 INC_FLAGS=-I$(ROOT)/include -I$(ROOT)/lib/CImg -I$(ROOT)/lib/libuv/include
 
+ifeq ($(Release), 1)
+C_FLAGS=-O2 -g $(INC_FLAGS) -fpermissive
+else
 C_FLAGS=-O0 -g $(INC_FLAGS) -fpermissive
+endif
 
 HAS_LIBUV = $(shell ls -d $(ROOT)/lib/libuv 2>/dev/null)
 
@@ -10,6 +14,12 @@ LIBMUG=$(ROOT)/bin/libmug.a
 
 ifneq ($(HAS_LIBUV), )
 C_FLAGS+= -DUSE_LIBUV
-LD_FLAGS+=-L$(ROOT)/lib/libuv/out/Debug -L$(ROOT)/lib/libuv/out/Release -luv
+
+ifeq ($(Release), 1)
+LD_FLAGS+=-L$(ROOT)/lib/libuv/out/Release -luv
+else
+LD_FLAGS+=-L$(ROOT)/lib/libuv/out/Debug -luv
+endif
+
 else
 endif
