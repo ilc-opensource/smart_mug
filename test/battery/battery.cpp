b3828ec13946d0f397f34085bf1bb29cd89535f9
Zhanglin Liu <zliu9@zliu9-mac01.bj.intel.com>
2014-12-23 17:24:10 +0800

    - display battery as colored bar`

--- test/battery/battery.cpp
+++ test/battery/battery.cpp
@@ -14,6 +14,8 @@ CImg<unsigned char> canvas(SCREEN_WIDTH, SCREEN_HEIGHT, 1, 3, 0);
 
 #define WARM 30
 #define HOT  50
+#define BAR_NUM 10
+#define BAR_STEP (100 / BAR_NUM)
 
 handle_t disp_handle;
 handle_t battery_handle;
@@ -39,17 +41,36 @@ void draw_data(int t, char *c)
   mug_disp_cimg(disp_handle, (cimg_handle_t)&canvas);
 }
 
-void on_battery(int percent, bool is_charge)
+void draw_bar(int percent, int is_charge)
+{ 
+  int normal = (percent - (percent % BAR_STEP));
+
+  printf("normal to %d\n", normal);
+  
+  char f[128];
+
+  sprintf(f, "%s_%02d.bmp", is_charge ? "charge" : "discharge", normal);
+
+  mug_disp_img(disp_handle, f);
+}
+
+void on_battery(int percent, int is_charge)
 {
+  printf("%s: %02d\n", is_charge? "charge": "discharge", percent);
+#if 0
   printf("%d\n", percent);
   if(is_charge)
     draw_data(percent, "green");
   else
     draw_data(percent, "magenta");
+#else
+  draw_bar(percent, is_charge);
+#endif
 }
 
 int main(int argc, char** argv)
 {
+  printf("init ok\n");
   disp_handle = mug_disp_init();
   battery_handle = mug_battery_init();
 
